bc1820e83b857fd54ed472a147fae82951c42a9e
Anton Hladkyi <gladkyy@ukr.net>
2020-05-23 18:40:22 +0300

    Added State Machine and Attack Animations
    added state machine for walking attacking and interacting(not used now) and also implemented attack animations using keybind(space)

--- OOP Game/Assets/Scripts/PlayerMovement.cs
+++ OOP Game/Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact
+}
 public class PlayerMovement : MonoBehaviour
 {
+    public PlayerState currentState;
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
@@ -12,8 +19,11 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentState = PlayerState.walk;
         animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
     }
 
     // Update is called once per frame
@@ -23,9 +33,26 @@ public class PlayerMovement : MonoBehaviour
         // Maybe decide later to change to GetAxis (will add fluent accelaration)
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        UpdateAnimationAndMove();
+        if (Input.GetButtonDown("Attack") && currentState!=PlayerState.attack)
+        {
+            StartCoroutine(AttackCo());
+        }
+        else if(currentState==PlayerState.walk)
+        {
+            UpdateAnimationAndMove();
+        }
     }
 
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("Attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("Attacking", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
+
+    }
     void UpdateAnimationAndMove()
     {
         if (change != Vector3.zero)
@@ -43,6 +70,7 @@ public class PlayerMovement : MonoBehaviour
 
     void MoveCharacter()
     {
+        change.Normalize();
         myRigidbody.MovePosition(transform.position + change.normalized * speed * Time.deltaTime);
     }
 }
