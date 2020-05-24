38ce0adf2d8393dd457cf843fe3c7f3c0c9e5b9e
Rostyslav M <mrostyk55@gmail.com>
2020-05-24 23:04:17 +0300

    Log script, added chasing to Log

--- OOP Game/Assets/Scripts/Enemy.cs
+++ OOP Game/Assets/Scripts/Enemy.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int Health;
+    public string EnemyName;
+    public int BaseAttack;
+    public float MoveSpeed;
     // Start is called before the first frame update
     void Start()
     {
