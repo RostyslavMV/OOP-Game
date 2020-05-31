8f80b174c2ae7cbb58fba372ca2abd5861fa64de
Anton Hladkyi <gladkyy@ukr.net>
2020-05-31 14:42:35 +0300

    added coins and coin tracker in ui and inventory also created prefabs for them

--- OOP Game/Assets/Scripts/ScriptableObject/Inventory.cs
+++ OOP Game/Assets/Scripts/ScriptableObject/Inventory.cs
@@ -8,7 +8,7 @@ public class Inventory : ScriptableObject
     public Item currentItem;
     public List<Item> items = new List<Item>();
     public int numberOfkeys;
-
+    public int coins;
     public void AddItem(Item itemToAdd)
     {
         //is the item a key
